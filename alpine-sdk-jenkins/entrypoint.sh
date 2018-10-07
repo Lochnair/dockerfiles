f495e6216967a2a40204329d97cf1afb6a154f03
Nils Andreas Svee <me@lochnair.net>
2018-10-07 17:36:54 +0200

    alpine-sdk-jenkins: add sdk to abuild

--- alpine-sdk-jenkins/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-PUID=$(id -u)
-PGID=$(id -g)
-su-exec root sh -c "usermod -u $PUID sdk"
-su-exec root sh -c "groupmod -g $PGID sdk"
-su-exec root sh -c "chown -R sdk:sdk /home/sdk"
-exec "$@"
