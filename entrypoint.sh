069ed55b0789291d9145b460dd2e4d362bdb7fd4
Nils Andreas Svee <nils@stokkdalen.no>
2019-10-06 19:09:33 +0200

    opensmtpd_alpine: fix spool permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,20 @@ usermod -u "$DAEMON_UID" smtpd
 
 groupmod -g "$QUEUE_GID" smtpq
 usermod -u "$QUEUE_UID" smtpq
-chown -R smtpq:smtpq /var/spool/smtpd
+
+mkdir -p /var/spool/smtpd
+mkdir -p /var/spool/smtpd/purge
+mkdir -p /var/spool/smtpd/queue
+mkdir -p /var/spool/smtpd/corrupt
+mkdir -p /var/spool/smtpd/offline
+
+chown -R root.root /var/spool/smtpd
+chmod 0711 /var/spool/smtpd
+chown -R smtpd.root /var/spool/smtpd/purge /var/spool/smtpd/queue /var/spool/smtpd/corrupt
+chmod 0700 /var/spool/smtpd/purge
+chown -R root.smtpq /var/spool/smtpd/offline
+chmod 0770 /var/spool/smtpd/offline
+chmod 0700 /var/spool/smtpd/queue /var/spool/smtpd/corrupt
 
 test -f /etc/smtpd/smtpd.conf || cp -v /etc/smtpd.dist/smtpd.conf /etc/smtpd/
 test -f /etc/smtpd/aliases || cp -v /etc/smtpd.dist/aliases /etc/smtpd/
